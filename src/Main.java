1a1d403967331574ba0015a27a5cd449b99b6a71
User <amantursuerov166@gmail.com>
2023-08-23 17:31:43 +0600

    df

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Rectangle b =new Rectangle("10sm","15sm");
+        System.out.println(b.height+" "+b.length);
+
+
+    }
+}
---
+++ src/Rectangle.java
@@ -0,0 +1,10 @@
+public class Rectangle {
+    String height;
+    String length;
+    public Rectangle(String height ,String length) {
+        this.height = height;
+        this.length =length;
+    }
+}
+
+
